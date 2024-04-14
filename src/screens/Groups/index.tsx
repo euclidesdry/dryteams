891528550f732d363f4a4c188e8d48154b4daf1d
Euclides do Rosário <euclidesdry@gmail.com>
2024-04-14 21:21:23 +0200

    feat(class): ✨ listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "~components/Header";
 import { Highlight } from "~components/Highlight";
 
@@ -5,13 +8,19 @@ import { Container } from "./styles";
 import { GroupCard } from "~components/GroupCard";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
