9bda225dd3bf6eb8eeaee401b34c77731913bd7f
Euclides do Rosário <euclidesdry@gmail.com>
2024-04-15 09:42:21 +0200

    feat(class): ✨ adding component ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 
 import { Header } from "~components/Header";
 import { Highlight } from "~components/Highlight";
 
 import { Container } from "./styles";
 import { GroupCard } from "~components/GroupCard";
+import { ListEmpty } from "~components/ListEmpty";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
@@ -18,9 +19,20 @@ export function Groups() {
 
       <FlatList
         data={groups}
+        style={styles.cardList}
         keyExtractor={(item) => String(item)}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  cardList: {
+    width: "100%",
+  },
+});
