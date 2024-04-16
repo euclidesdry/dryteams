ea3110355a6d2759abe50ff02824c580e065e816
Euclides do Rosário <euclidesdry@gmail.com>
2024-04-16 20:51:36 +0200

    refactor(class): ♻️ using css helper

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,7 @@
+export * from "./Button";
+export * from "./GroupCard";
 export * from "./Header";
 export * from "./Highlight";
+export * from "./Input";
+export * from "./ListEmpty";
 export * from "./Loading";
